c07c364086f6dd47b36567680d3ca15b29302b90
mohit <mdmohitdhingra9@gmail.com>
2022-03-01 13:35:38 +0530

    msg

--- SampleTCApp/Program.cs
+++ SampleTCApp/Program.cs
@@ -10,7 +10,7 @@ namespace SampleTCApp
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hi! Welcome.");
+            Console.WriteLine("Hi! Welcome");
 
             Console.Read();
         }
